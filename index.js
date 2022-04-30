5a0c3e50b913f9ee69a94fdd070dea10ef6d7558
razuahmedjoy <rjjoybzs420@gmail.com>
2022-04-30 19:54:36 +0600

    Database connected and checked

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 const express = require('express');
+
+// added it for cors 
+const cors = require('cors');
+
+// added to configure secret keys
+require('dotenv').config()
+
 const app = express();
 const port = process.env.PORT || 8000
 
+app.use(cors());
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send("Hello server running");
-})
 
-app.listen(port,()=>{
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mwock.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function dbConnect(){
+    try{
+
+        await client.connect();
+        const inventoryCollection = client.db("gearHouse").collection("inventory");
+
+        // all apis
+        app.get('/inventories', async (req,res)=>{
+            const query = {};
+
+            const cursor = inventoryCollection.find(query);
+
+            const inventories = await cursor.limit(6).toArray();
+            res.send(inventories);
+        })
+
+        app.get('/', (req, res) => {
+            res.send("Hello server running and database connected");
+        })
+
+    }
+    finally{
+       
+    }
+}
+
+dbConnect().catch(console.dir)
+
+
+
+app.listen(port, () => {
     console.log(`Server running at following url http://localhost:${port}`)
 });
